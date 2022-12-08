4e6dcac9988a506eff99fb1848cab3879d6114dc
Paul Miell <paulm@mfg.gg>
2022-12-08 16:03:24 +0200

    Revert "Merge branch 'main' of https://github.com/Made-For-Gamers/Asteroid-Unity"
    This reverts commit 0b0bf181ee1bc37dae2a379e945383d3c6e4e798, reversing
    changes made to 42fc371c6b45aeefd7a7284a22aed707507a1c36.

--- Asteroid-Unity/Assets/Scripts/General/AsteroidSpawner.cs
+++ Asteroid-Unity/Assets/Scripts/General/AsteroidSpawner.cs
@@ -2,18 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-/// <summary>
-/// Spawning of random asteroids at set intervals, random rotation and speed, targets an object for direction
-/// </summary>
 public class AsteroidSpawner : MonoBehaviour
 {
-    [SerializeField] private SO_GameObjects asteroids;
-    [SerializeField] private float spawnTime;
-    [SerializeField] private float speedMin;
-    [SerializeField] private float speedMax;
-    [SerializeField] private GameObject target;
-    private float speed;
+    [SerializeField] SO_Asteroids asteroids;
+    [SerializeField] float spawnTime;
+    [SerializeField] float speedMin;
+    [SerializeField] float speedMax;
+    [SerializeField] GameObject target;
+    float speed;
 
     private void Start()
     {
@@ -22,16 +18,10 @@ public class AsteroidSpawner : MonoBehaviour
 
     private void SpawnAsteroid()
     {
-        //Instantiate a new asteroid    
-        GameObject spawnedAsteroid = Instantiate(asteroids.GetRandomGameObject());
-        spawnedAsteroid.transform.position = transform.position;
+        int randomAsteroid = Random.Range(0, asteroids.Asteroids.Count);
+        GameObject spawnedAsteroid = Instantiate(asteroids.Asteroids[randomAsteroid], transform.position, transform.rotation);
+        spawnedAsteroid.transform.LookAt(target.transform);
         speed = Random.Range(speedMin, speedMax + 1);
-
-        //Look and move towards target
-        spawnedAsteroid.transform.LookAt(target.transform); 
         spawnedAsteroid.GetComponent<Rigidbody>().velocity = spawnedAsteroid.transform.forward * speed;
-
-        //Random rotation after Look and direction is set
-        spawnedAsteroid.transform.rotation = Random.rotation; 
     }
 }
