ceab4333c0b5f5a6b4c600eeb7162cf14e224e5b
Paul Miell <paulm@mfg.gg>
2022-12-08 15:17:49 +0200

    Bullet detection / asteroid splitting

--- Asteroid-Unity/Assets/Scripts/General/Bullet.cs
+++ Asteroid-Unity/Assets/Scripts/General/Bullet.cs
@@ -2,9 +2,44 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    //Destroy bullets when leaving the screen
-    private void OnBecameInvisible()
+    [SerializeField] GameObject explosion;
+    [SerializeField] SO_Player player;
+    [SerializeField] SO_GameObjects asteroids;
+    GameObject explosionObject;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        Destroy(this.gameObject);
+        print("Collision: " + collision.gameObject.tag);
+        switch (collision.gameObject.tag)
+        {            
+            case "Asteroid":
+                gameObject.GetComponent<BoxCollider>().enabled = false;
+                player.Score += 100;
+               // explosionObject = Instantiate(explosion, collision.transform.position, collision.transform.rotation);
+                Destroy(explosionObject, 1.5f);
+                Vector3 position = collision.transform.position;
+                Vector3 scale = collision.transform.localScale;
+                Destroy(collision.gameObject);
+                if (scale.x > 0.25)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        GameObject spawnedAsteroid = Instantiate(asteroids.Obj[Random.Range(0, asteroids.Obj.Count)], position, transform.rotation);
+                        spawnedAsteroid.transform.localScale = new Vector3(scale.x / 2, scale.y / 2, scale.z / 2);
+                    }
+                }
+                Destroy(gameObject);
+                break;
+
+            case "P1":
+            case "P2":
+                player.Score += 1000;
+                explosionObject = Instantiate(explosion, collision.transform.position, collision.transform.rotation);
+                Destroy(explosionObject, 1.5f);
+                collision.transform.position = new Vector3(0, 0, 0);
+                Destroy(gameObject);
+                break;
+        }
+
     }
 }
