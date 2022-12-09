afe15412a3f4e0ca37d2ef1b8e8772dbd2c822d5
Paul Miell <paulm@mfg.gg>
2022-12-09 06:57:15 +0200

    Revert "Revert "Merge branch 'main' of https://github.com/Made-For-Gamers/Asteroid-Unity""
    This reverts commit 4e6dcac9988a506eff99fb1848cab3879d6114dc.

--- Asteroid-Unity/Assets/Scripts/ScriptableObjects/SO_Player.cs
+++ Asteroid-Unity/Assets/Scripts/ScriptableObjects/SO_Player.cs
@@ -8,13 +8,20 @@ public class SO_Player : ScriptableObject
     [TextArea(5, 5)]
     [SerializeField] private string description;
     [SerializeField] private int xp;
+    [SerializeField] private int score;
+
+    public string CharName { get => charName; set => charName = value; } 
+    public string Description { get => description; set => description = value; } 
+    public int Xp { get => xp; set => xp = value; }
+    public int Score { get => score; set => score = value; }
+
 
     //Editor change/update
     private void OnValidate()
     {
-        if (string.IsNullOrEmpty(charName))
+        if (string.IsNullOrEmpty(CharName))
         {
-            charName = name;
+            CharName = name;
         }
     }
 }
