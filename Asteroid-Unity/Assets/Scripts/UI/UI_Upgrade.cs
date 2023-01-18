6ed67a81bc3fcfc7efe96b54a340c143fad7d012
Paul Miell <paulm@mfg.gg>
2023-01-18 13:26:31 +0200

    Completed upgrade script
    Shield/speed/bullet 2 levels of upgrade
    Purchase upgrades using iterium

--- Asteroid-Unity/Assets/Scripts/UI/UI_Upgrade.cs
+++ Asteroid-Unity/Assets/Scripts/UI/UI_Upgrade.cs
@@ -4,30 +4,129 @@ using UnityEditor.UIElements;
 
 public class UI_Upgrade : MonoBehaviour
 {
-    [Header("UI Slider names")]
+    [Header("UI Upgrade Sliders")]
     [SerializeField] private string thrustSlider = "thrustSlider";
     [SerializeField] private string shieldSlider = "shieldSlider";
     [SerializeField] private string firepowerSlider = "firepowerSlider";
 
-    [Header("Thrust Iterium Requirement")]
-    [SerializeField] private int level1 = 10;
-    [SerializeField] private int level2 = 20;
-    [SerializeField] private int level3 = 30;
+    [Header("UI Upgrade Buttons")]
+    [SerializeField] private string thrustButton = "thrustUpgrade";
+    [SerializeField] private string shieldButton = "shieldUpgrade";
+    [SerializeField] private string firepowerButton = "firepowerUpgrade";
+
+    [Header("UI Iterium Label")]
+    [SerializeField] private string iteriumLabel = "iteriumAmount";
+
+    [Header("Thrust Iterium Requirement")]   
+    [SerializeField] private int thrustLevel1 = 10;
+    [SerializeField] private int thrustLevel2 = 30;
+
+    [Header("Shield Iterium Requirement")]  
+    [SerializeField] private int shieldLevel1 = 10;
+    [SerializeField] private int shieldLevel2 = 30;
+
+    [Header("Firepower Iterium Requirement")]   
+    [SerializeField] private int firepowerLevel1 = 10;
+    [SerializeField] private int firepowerLevel2 = 30;
 
     private Slider thrust;
-    private Button upgrade;
+    private Button thrustUpgrade;
+    private Slider shield;
+    private Button shieldUpgrade;
+    private Slider firepower;
+    private Button firepowreUpgrade;
+    private Label iterium;
 
     private void OnEnable()
     {
+        //UI Toolkit Document
         VisualElement uiRoot = GetComponent<UIDocument>().rootVisualElement;
+        iterium = uiRoot.Q<Label>(iteriumLabel);
+
+
+        //UI thrust elements
         thrust = uiRoot.Q<Slider>(thrustSlider);
-        upgrade = uiRoot.Q<Button>("thrustUpgrade");
+        thrustUpgrade = uiRoot.Q<Button>(thrustButton);
         thrust.SetEnabled(false);
-        upgrade.clicked += UpgradeThrust;
+        thrustUpgrade.clicked += UpgradeThrust;
+
+        //UI shield elements
+        shield = uiRoot.Q<Slider>(shieldSlider);
+        shieldUpgrade = uiRoot.Q<Button>(shieldButton);
+        shield.SetEnabled(false);
+        shieldUpgrade.clicked += UpgradeShield;
+
+        //UI firepower elements
+        firepower = uiRoot.Q<Slider>(firepowerSlider);
+        firepowreUpgrade = uiRoot.Q<Button>(firepowerButton);
+        firepower.SetEnabled(false);
+        firepowreUpgrade.clicked += UpgradeFirepower;
+    }
+
+    private void Start()
+    {
+        ChangeIterium();
+        GameManager.Instance.player.onChange_Iterium.AddListener(ChangeIterium);
     }
 
     void UpgradeThrust()
     {
-        thrust.value = 2;
+        if (GameManager.Instance.player.SpeedLvl == 0 && GameManager.Instance.player.Iterium >= thrustLevel1)
+        {
+            //Upgrade speed to lvl 1
+            GameManager.Instance.player.Iterium -= thrustLevel1;
+            GameManager.Instance.player.SpeedLvl = 1;
+            thrust.value = 1;
+        }
+        else if (GameManager.Instance.player.SpeedLvl == 1 && GameManager.Instance.player.Iterium >= thrustLevel2)
+        {
+            //Upgrade speed to lvl 2
+            GameManager.Instance.player.Iterium -= thrustLevel2;
+            GameManager.Instance.player.SpeedLvl = 2;
+            thrust.value = 2;
+        }
     }
+
+    void UpgradeShield()
+    {
+        if (GameManager.Instance.player.ShieldLvl == 0 && GameManager.Instance.player.Iterium >= shieldLevel1)
+        {
+            //Upgrade shield to lvl 1
+            GameManager.Instance.player.Iterium -= shieldLevel1;
+            GameManager.Instance.player.ShieldLvl = 1;
+            shield.value = 1;
+        }
+        else if (GameManager.Instance.player.ShieldLvl == 1 && GameManager.Instance.player.Iterium >= shieldLevel2)
+        {
+            //Upgrade shield to lvl 1
+            GameManager.Instance.player.Iterium -= shieldLevel2;
+            GameManager.Instance.player.ShieldLvl = 2;
+            shield.value = 2;
+        }
+    }
+
+    void UpgradeFirepower()
+    {
+        if (GameManager.Instance.player.BulletLvl == 0 && GameManager.Instance.player.Iterium >= firepowerLevel1)
+        {
+            //Upgrade bullet to lvl 1
+            GameManager.Instance.player.Iterium -= firepowerLevel1;
+            GameManager.Instance.player.BulletLvl = 1;
+            firepower.value = 1;
+        }
+        else if (GameManager.Instance.player.BulletLvl == 1 && GameManager.Instance.player.Iterium >= firepowerLevel2)
+        {
+            //Upgrade bullet to lvl 2
+            GameManager.Instance.player.Iterium -= firepowerLevel2;
+            GameManager.Instance.player.BulletLvl = 2;
+            firepower.value = 2;
+        }
+    }
+
+    void ChangeIterium()
+    {
+        iterium.text = GameManager.Instance.player.Iterium.ToString();
+    }
+
+
 }
