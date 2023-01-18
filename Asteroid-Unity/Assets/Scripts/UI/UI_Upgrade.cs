b8610e1befa7429a60b2b153a49b8b2721a0e3c8
Paul Miell <paulm@mfg.gg>
2023-01-18 14:16:06 +0200

    Added label change according to inspector requirement field values

--- Asteroid-Unity/Assets/Scripts/UI/UI_Upgrade.cs
+++ Asteroid-Unity/Assets/Scripts/UI/UI_Upgrade.cs
@@ -17,24 +17,43 @@ public class UI_Upgrade : MonoBehaviour
     [Header("UI Iterium Label")]
     [SerializeField] private string iteriumLabel = "iteriumAmount";
 
-    [Header("Thrust Iterium Requirement")]   
+    [Header("Thrust Iterium Requirement")]
     [SerializeField] private int thrustLevel1 = 10;
+    [SerializeField] private string thrustLevel1Label = "thrustLevel1";
     [SerializeField] private int thrustLevel2 = 30;
+    [SerializeField] private string thrustLevel2Label = "thrustLevel2";
 
-    [Header("Shield Iterium Requirement")]  
+    [Header("Shield Iterium Requirement")]
     [SerializeField] private int shieldLevel1 = 10;
+    [SerializeField] private string shieldLevel1Label = "shieldLevel1";
     [SerializeField] private int shieldLevel2 = 30;
+    [SerializeField] private string shieldLevel2Label = "shieldLevel2";
 
-    [Header("Firepower Iterium Requirement")]   
+    [Header("Firepower Iterium Requirement")]
     [SerializeField] private int firepowerLevel1 = 10;
+    [SerializeField] private string firepowerLevel1Label = "firepowerLevel1";
     [SerializeField] private int firepowerLevel2 = 30;
+    [SerializeField] private string firepowerLevel2Label = "firepowerLevel2";
 
+    //UI thrust controls
     private Slider thrust;
     private Button thrustUpgrade;
+    private Label thrustLabelLevel1;
+    private Label thrustLabelLevel2;
+
+    //UI shield controls
     private Slider shield;
     private Button shieldUpgrade;
+    private Label shieldLabelLevel1;
+    private Label shieldLabelLevel2;
+
+    //UI firepower controls
     private Slider firepower;
     private Button firepowreUpgrade;
+    private Label firepowerLabelLevel1;
+    private Label firepowerLabelLevel2;
+
+    //UI iterium label
     private Label iterium;
 
     private void OnEnable()
@@ -47,24 +66,31 @@ public class UI_Upgrade : MonoBehaviour
         //UI thrust elements
         thrust = uiRoot.Q<Slider>(thrustSlider);
         thrustUpgrade = uiRoot.Q<Button>(thrustButton);
+        thrustLabelLevel1 = uiRoot.Q<Label>(thrustLevel1Label);
+        thrustLabelLevel2 = uiRoot.Q<Label>(thrustLevel2Label);
         thrust.SetEnabled(false);
         thrustUpgrade.clicked += UpgradeThrust;
 
         //UI shield elements
         shield = uiRoot.Q<Slider>(shieldSlider);
         shieldUpgrade = uiRoot.Q<Button>(shieldButton);
+        shieldLabelLevel1 = uiRoot.Q<Label>(shieldLevel1Label);
+        shieldLabelLevel2 = uiRoot.Q<Label>(shieldLevel2Label);
         shield.SetEnabled(false);
         shieldUpgrade.clicked += UpgradeShield;
 
         //UI firepower elements
         firepower = uiRoot.Q<Slider>(firepowerSlider);
         firepowreUpgrade = uiRoot.Q<Button>(firepowerButton);
+        firepowerLabelLevel1 = uiRoot.Q<Label>(firepowerLevel1Label);
+        firepowerLabelLevel2 = uiRoot.Q<Label>(firepowerLevel2Label);
         firepower.SetEnabled(false);
         firepowreUpgrade.clicked += UpgradeFirepower;
     }
 
     private void Start()
     {
+        ChangeUpgradeLabels();
         ChangeIterium();
         GameManager.Instance.player.onChange_Iterium.AddListener(ChangeIterium);
     }
@@ -128,5 +154,13 @@ public class UI_Upgrade : MonoBehaviour
         iterium.text = GameManager.Instance.player.Iterium.ToString();
     }
 
-
+    void ChangeUpgradeLabels()
+    {
+        thrustLabelLevel1.text = thrustLevel1.ToString();
+        thrustLabelLevel2.text = thrustLevel2.ToString();
+        shieldLabelLevel1.text = shieldLevel1.ToString();
+        shieldLabelLevel2.text = shieldLevel2.ToString();
+        firepowerLabelLevel1.text = firepowerLevel1.ToString();
+        firepowerLabelLevel2.text = firepowerLevel2.ToString();
+    }
 }
