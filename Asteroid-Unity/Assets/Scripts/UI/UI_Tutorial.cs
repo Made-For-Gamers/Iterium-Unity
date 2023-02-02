073372caad5bfd5980b187d6b2a40c400aec302f
Paul Miell <paulm@mfg.gg>
2023-02-02 16:59:05 +0200

    Connected Tutorial UI elements

--- Asteroid-Unity/Assets/Scripts/UI/UI_Tutorial.cs
+++ Asteroid-Unity/Assets/Scripts/UI/UI_Tutorial.cs
@@ -1,46 +1,99 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem.Controls;
 using UnityEngine.UIElements;
 
 public class UI_Tutorial : MonoBehaviour
 {
     [Header("UI Elements")]
-    [SerializeField] private string factions;
-    [SerializeField] private string upgrades;
-    [SerializeField] private string iterium;
-    [SerializeField] private string gameplay;
+    [SerializeField] private string story = "buttonStory";
+    [SerializeField] private string controls = "controls";
+    [SerializeField] private string gameplay = "gameplay";
+    [SerializeField] private string scoring = "scoring";
+    [SerializeField] private string upgrades = "upgrades";
+
+    [SerializeField] private string storyPanel = "storyPanel";
+    [SerializeField] private string controlsPanel = "controlsPanel";
+    [SerializeField] private string gameplayPanel = "gameplayPanel";
+    [SerializeField] private string scoringPanel = "scoringPanel";
+    [SerializeField] private string upgradesPanel = "upgradesPanel";
+
+    Button buttonStory;
+    Button buttonControls;
+    Button buttonGameplay;
+    Button buttonScoring;
+    Button buttonUpgrades;
+
+    VisualElement panelStory;
+    VisualElement panelControls;
+    VisualElement panelGameplay;
+    VisualElement panelScoring;
+    VisualElement panelUpgrades;
 
     private void OnEnable()
     {
         VisualElement uiRoot = GetComponent<UIDocument>().rootVisualElement;
-        Button factionsButton = uiRoot.Q<Button>(factions);
-        Button upgradesButton = uiRoot.Q<Button>(factions);
-        Button iteriumButton = uiRoot.Q<Button>(factions);
-        Button gameplayButton = uiRoot.Q<Button>(factions);
+        
+        //UI buttons
+        buttonStory = uiRoot.Q<Button>(story);
+        buttonControls = uiRoot.Q<Button>(controls);
+        buttonGameplay = uiRoot.Q<Button>(gameplay);
+        buttonScoring = uiRoot.Q<Button>(scoring);
+        buttonUpgrades = uiRoot.Q<Button>(upgrades);
 
-        //Click events
-        factionsButton.clicked += factions_clicked;
-        upgradesButton.clicked += upgrades_clicked;
-        iteriumButton.clicked += iterium_clicked;
-        gameplayButton.clicked += gameplay_clicked;
+        //Visual elements
+        panelStory = uiRoot.Q<VisualElement>(storyPanel);
+        panelControls = uiRoot.Q<VisualElement>(controlsPanel);
+        panelGameplay = uiRoot.Q<VisualElement>(gameplayPanel);
+        panelScoring = uiRoot.Q<VisualElement>(scoringPanel);
+        panelUpgrades = uiRoot.Q<VisualElement>(upgradesPanel);
+
+        //Button click events
+        buttonStory.clicked += StoryClicked;
+        buttonControls.clicked += ControlsClicked;
+        buttonGameplay.clicked += GameplayClicked;
+        buttonScoring.clicked += ScoringClicked;
+        buttonUpgrades.clicked += UpgradesClicked;
+
+        buttonStory.Focus();
     }
 
-   private void factions_clicked()
+    private void UpgradesClicked()
     {
-        print("clicked");
+        HideAllPanels();
+        panelUpgrades.style.display = DisplayStyle.Flex;
     }
 
-    private void upgrades_clicked()
+    private void ScoringClicked()
     {
-        print("clicked");
-    }
-   private void iterium_clicked()
-    {
-        print("clicked");
+        HideAllPanels();
+        panelScoring.style.display = DisplayStyle.Flex;
     }
 
-    private void gameplay_clicked()
+    private void GameplayClicked()
     {
-        print("clicked");
+        HideAllPanels();
+        panelGameplay.style.display = DisplayStyle.Flex;
+    }
+
+    private void ControlsClicked()
+    {
+        HideAllPanels();
+        panelControls.style.display = DisplayStyle.Flex;
+    }
+
+    private void StoryClicked()
+    {
+        HideAllPanels();
+        panelStory.style.display = DisplayStyle.Flex;
+    }
+
+    private void HideAllPanels()
+    { 
+        panelStory.style.display = DisplayStyle.None;
+        panelControls.style.display = DisplayStyle.None;
+        panelGameplay.style.display = DisplayStyle.None;
+        panelScoring.style.display = DisplayStyle.None;
+        panelUpgrades.style.display = DisplayStyle.None;
     }
 }
